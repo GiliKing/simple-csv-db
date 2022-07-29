e764a25014d623b515368f0318cf70cf0290a6a1
giliking <chrisogili12@gmail.com>
2022-07-28 23:09:25 -0700

    first commit

--- config/Database.php
+++ config/Database.php
@@ -2,10 +2,10 @@
 
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "money";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b1672a420eb63d";
+$password = "a6e5c671";
+$db = "heroku_709f95d041c95ca";
 
 $conn = mysqli_connect($servername, $username, $password, $db) or die("Could not connect");
 
